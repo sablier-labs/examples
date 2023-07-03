6bfdf0dbc938923f50c5220ab2163eaa6cda0bc5
andreivladbrg <andreivladbrg@gmail.com>
2023-07-03 18:00:53 +0300

    feat: add linear batch example
    feat: add dynamic batch example
    feat: add test for batch linear example
    fix: use weeks instead of years
    fix: declare dynamic segments correctly

---
+++ v2/periphery/LockupDynamicBatchStreamCreator.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.13;
+
+import { IAllowanceTransfer } from "@permit2/interfaces/IAllowanceTransfer.sol";
+import { ISablierV2ProxyTarget } from "@sablier/v2-periphery/interfaces/ISablierV2ProxyTarget.sol";
+import { Batch, Broker, LockupDynamic, Permit2Params } from "@sablier/v2-periphery/types/DataTypes.sol";
+import { BatchBuilder } from "@sablier/v2-periphery-test/utils/BatchBuilder.sol";
+import { ISablierV2LockupDynamic } from "@sablier/v2-core/interfaces/ISablierV2LockupDynamic.sol";
+import { ud2x18, ud60x18 } from "@sablier/v2-core/types/Math.sol";
+import { IERC20 } from "@sablier/v2-core/types/Tokens.sol";
+
+contract LockupDynamicBatchStreamCreator {
+    IERC20 public constant DAI = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+    IAllowanceTransfer public constant PERMIT2 = IAllowanceTransfer(0x000000000022D473030F116dDEE9F6B43aC78BA3);
+    IPRBProxyRegistry public constant PROXY_REGISTRY = IPRBProxyRegistry(0x33e200B5fb5e0C57d370d5202c26A35d07A46B98);
+    ISablierV2LockupDynamic public immutable sablierLockupDynamic;
+    ISablierV2ProxyTarget public immutable sablierProxyTarget;
+
+    constructor(ISablierV2LockupDynamic sablierLockupDynamic_, ISablierV2ProxyTarget sablierProxyTarget_) {
+        sablierLockupDynamic = sablierLockupDynamic_;
+        sablierProxyTarget = sablierProxyTarget_;
+    }
+
+    function batchCreateLockupDynamicStream(
+        uint256 perStreamAmount,
+        uint256 batchSize,
+        bytes memory signature
+    )
+        public
+        returns (uint256[] memory streamIds)
+    {
+        IPRBProxy proxy = PROXY_REGISTRY.getProxy({ owner: address(this) }); // Get the proxy for this contract
+        if (address(proxy) == address(0)) {
+            proxy = PROXY_REGISTRY.deployFor(address(this)); // Deploy the proxy if it doesn't exist
+        }
+
+        uint256 transferAmount = perStreamAmount * batchSize;
+
+        // Approve the Permit2 contract to spend DAI
+        uint256 allowance = DAI.allowance(address(this), address(PERMIT2));
+        if (allowance < transferAmount) {
+            DAI.approve({ spender: address(PERMIT2), amount: type(uint256).max });
+        }
+
+        IAllowanceTransfer.PermitDetails memory permitDetails;
+        permitDetails.token = address(DAI);
+        permitDetails.amount = uint160(transferAmount);
+        permitDetails.expiration = type(uint48).max; // maximum expiration possible
+        (,, permitDetails.nonce) =
+            PERMIT2.allowance({ user: address(this), token: address(DAI), spender: address(proxy) });
+
+        IAllowanceTransfer.PermitSingle memory permitSingle;
+        permitSingle.details = permitDetails;
+        permitSingle.sigDeadline = type(uint48).max; // same deadline as expiration
+
+        // Declare the Permit2 params
+        Permit2Params memory permit2Params;
+        permit2Params.permitSingle = permitSingle;
+        permit2Params.signature = signature;
+
+        // Declare a single batch struct
+        Batch.CreateWithMilestones memory batchSingle;
+        batchSingle.sender = address(proxy); // The sender will be able to cancel the stream
+        batchSingle.recipient = address(0xcafe); // The recipient of the streamed assets
+        batchSingle.cancelable = true; // Whether the stream will be cancelable or not
+        batchSingle.segments = new LockupDynamic.Segment[](2);
+        // Declare some dummy segments
+        batchSingle.segments[0] =
+            LockupDynamic.Segment({ amount: 0, exponent: ud2x18(1e18), milestone: uint40(block.timestamp + 4 weeks) });
+        batchSingle.segments[1] = LockupDynamic.Segment({
+            amount: perStreamAmount,
+            exponent: ud2x18(3.14e18),
+            milestone: uint40(block.timestamp + 52 weeks)
+        });
+        batchSingle.broker = Broker(address(0), ud60x18(0)); // Optional parameter left undefined
+
+        // Fill the batch param
+        Batch.CreateWithMilestones[] memory batch = BatchBuilder.fillBatch(batchSingle, batchSize);
+
+        // Encode the data for the proxy
+        bytes memory data = abi.encodeCall(
+            sablierProxyTarget.batchCreateWithMilestones, (sablierLockupDynamic, DAI, batch, permit2Params)
+        );
+        // Create multiple streams via the proxy
+        bytes memory response = proxy.execute(address(sablierProxyTarget), data);
+        streamIds = abi.decode(response, (uint256[]));
+    }
+}
---
+++ v2/periphery/LockupLinearBatchStreamCreator.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: GPL-3-0-or-later
+pragma solidity >=0.8.13;
+
+import { ISablierV2ProxyTarget } from "@sablier/v2-periphery/interfaces/ISablierV2ProxyTarget.sol";
+import { ArrayBuilder } from "@sablier/v2-periphery-test/utils/ArrayBuilder.sol";
+import { ISablierV2LockupLinear } from "@sablier/v2-core/interfaces/ISablierV2LockupLinear.sol";
+import { Test } from "forge-std/Test.sol";
+
+import { LockupLinearBatchStreamCreator } from "./LockupLinearBatchStreamCreator.sol";
+
+contract LockupLinearBatchCreateTest is Test {
+    // Get the latest deployment address from the docs
+    // https://docs.sablier.com/contracts/v2/addresses
+    address internal constant SABLIER_ADDRESS = address(0xcafe);
+    address internal constant SABLIER_TARGET_ADDRESS = address(0xbeef);
+
+    // Test contracts
+    LockupLinearBatchCreate internal creator;
+    ISablierV2LockupLinear internal sablier;
+    ISablierV2ProxyTarget internal sablierTarget;
+
+    function setUp() public {
+        // Fork Ethereum Mainnet
+        vm.createSelectFork({ urlOrAlias: "mainnet" });
+
+        // Load the Sablier contract from Ethereum Mainnet
+        sablier = ISablierV2LockupLinear(SABLIER_ADDRESS);
+        sablierTarget = ISablierV2ProxyTarget(SABLIER_TARGET_ADDRESS);
+
+        // Deploy the stream creator
+        creator = new LockupLinearBatchCreate(sablier, sablierTarget);
+
+        // Mint some DAI tokens to the creator contract using the `deal` cheatcode
+        deal({ token: address(creator.DAI()), to: address(creator), give: 1337e18 });
+    }
+
+    // Tests that creating streams works by checking the stream ids
+    function test_batchCreateLockupLinearStream() public {
+        uint256 nextStreamId = sablier.nextStreamId();
+        uint256[] memory expectedStreamIds = ArrayBuilder.fillStreamIds(nextStreamId, 10);
+        uint256[] memory actualStreamIds =
+            creator.batchCreateLockupLinearStream({ perStreamAmount: 1337e18, batchSize: 10, signature: "" });
+        assertEq(actualStreamIds, expectedStreamIds);
+    }
+}
