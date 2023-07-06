2e7d7ebd3d8d2615870f6cbb938706395425822d
andreivladbrg <andreivladbrg@gmail.com>
2023-07-06 18:39:26 +0300

    docs: add natspec for contract

--- v2/periphery/DeployProxyAndInstallPlugin.sol
+++ v2/periphery/DeployProxyAndInstallPlugin.sol
@@ -5,6 +5,9 @@ import { IPRBProxy } from "@prb/proxy/interfaces/IPRBProxy.sol";
 import { IPRBProxyRegistry } from "@prb/proxy/interfaces/IPRBProxyRegistry.sol";
 import { ISablierV2ProxyPlugin } from "@sablier/v2-periphery/interfaces/ISablierV2ProxyPlugin.sol";
 
+/// @notice Example of how to deploy a Proxy and install the Sablier plugin.
+/// @dev This code is referenced in the docs:
+/// https://docs.sablier.com/contracts/v2/guides/proxy-architecture/deployandinstallplugin
 contract ProxyDeployerAndPluginInstaller {
     IPRBProxyRegistry public constant PROXY_REGISTRY = IPRBProxyRegistry(0xD42a2bB59775694c9Df4c7822BfFAb150e6c699D);
     ISablierV2ProxyPlugin public immutable sablierProxyPlugin;
@@ -14,11 +17,13 @@ contract ProxyDeployerAndPluginInstaller {
     }
 
     function deployProxyAndInstallPlugin() public returns (IPRBProxy proxy) {
-        proxy = PROXY_REGISTRY.getProxy({ owner: address(this) }); // Get the proxy for this contract
+        // Get the proxy for this contract
+        proxy = PROXY_REGISTRY.getProxy({ owner: address(this) });
         if (address(proxy) == address(0)) {
-            proxy = PROXY_REGISTRY.deployAndInstallPlugin({ plugin: sablierProxyPlugin }); // Deploy the proxy if it
-                // doesn't exist and install the plugin
+            // Deploy the proxy if it doesn't exist and install the plugin
+            proxy = PROXY_REGISTRY.deployAndInstallPlugin({ plugin: sablierProxyPlugin });
         } else {
+            // If it exists then just install the plugin
             PROXY_REGISTRY.installPlugin({ plugin: sablierProxyPlugin });
         }
     }
