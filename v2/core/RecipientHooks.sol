2dc6f4e7cfb7e4fe327347058be08c0ad6894a48
andreivladbrg <andreivladbrg@gmail.com>
2024-06-10 16:59:57 +0300

    refactor: use custom errors instead of require

--- v2/core/RecipientHooks.sol
+++ v2/core/RecipientHooks.sol
@@ -4,6 +4,8 @@ pragma solidity >=0.8.19;
 import { ISablierV2LockupRecipient } from "@sablier/v2-core/src/interfaces/hooks/ISablierV2LockupRecipient.sol";
 
 abstract contract RecipientHooks is ISablierV2LockupRecipient {
+    error CallerNotSablierContract(address caller, address sablierContract);
+
     address public immutable sablierLockup;
 
     mapping(address => uint256) internal _balances;
@@ -31,7 +33,9 @@ abstract contract RecipientHooks is ISablierV2LockupRecipient {
     // Do something after a stream was renounced by the sender.
     function onStreamRenounced(uint256 streamId) external view {
         // Check: the caller is the lockup contract.
-        require(msg.sender == sablierLockup, "unauthorized");
+        if (msg.sender != sablierLockup) {
+            revert CallerNotSablierContract(msg.sender, sablierLockup);
+        }
 
         // Update the risk ratio.
         _updateRiskRatio({ nftId: streamId });
@@ -40,7 +44,9 @@ abstract contract RecipientHooks is ISablierV2LockupRecipient {
     // Do something after the sender or an NFT operator withdrew funds from a stream.
     function onStreamWithdrawn(uint256, /* streamId */ address caller, address, /* to */ uint128 amount) external {
         // Check: the caller is the lockup contract.
-        require(msg.sender == sablierLockup, "unauthorized");
+        if (msg.sender != sablierLockup) {
+            revert CallerNotSablierContract(msg.sender, sablierLockup);
+        }
 
         // Reduce the user's balance.
         _balances[caller] -= amount;
