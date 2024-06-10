fc887890501d08d651be522f5aa2774f573427ff
smol-ninja <shubhamy2015@gmail.com>
2024-06-10 14:01:47 +0100

    fix: include caller check on hook calls

--- v2/core/RecipientHooks.sol
+++ v2/core/RecipientHooks.sol
@@ -4,7 +4,13 @@ pragma solidity >=0.8.19;
 import { ISablierV2LockupRecipient } from "@sablier/v2-core/src/interfaces/hooks/ISablierV2LockupRecipient.sol";
 
 abstract contract RecipientHooks is ISablierV2LockupRecipient {
+    address public immutable sablierLockup;
+
     mapping(address => uint256) internal _balances;
+
+    constructor(address sablierLockup_) {
+        sablierLockup = sablierLockup_;
+    }
 
     // Do something after a stream was canceled by the sender.
     function onStreamCanceled(
@@ -13,20 +19,29 @@ abstract contract RecipientHooks is ISablierV2LockupRecipient {
         uint128 /* recipientAmount */
     )
         external
-        pure
+        view
     {
+        // Check: the caller is the lockup contract.
+        require(msg.sender == sablierLockup, "unauthorized");
+
         // Liquidate the user's position.
         _liquidate({ nftId: streamId });
     }
 
     // Do something after a stream was renounced by the sender.
-    function onStreamRenounced(uint256 streamId) external pure {
+    function onStreamRenounced(uint256 streamId) external view {
+        // Check: the caller is the lockup contract.
+        require(msg.sender == sablierLockup, "unauthorized");
+
         // Update the risk ratio.
         _updateRiskRatio({ nftId: streamId });
     }
 
     // Do something after the sender or an NFT operator withdrew funds from a stream.
     function onStreamWithdrawn(uint256, /* streamId */ address caller, address, /* to */ uint128 amount) external {
+        // Check: the caller is the lockup contract.
+        require(msg.sender == sablierLockup, "unauthorized");
+
         // Reduce the user's balance.
         _balances[caller] -= amount;
     }
