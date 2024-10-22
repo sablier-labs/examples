8bb3f4cb582c85028f6de691ca8392691c8ba03d
andreivladbrg <andreivladbrg@gmail.com>
2024-10-22 13:22:43 +0300

    fix: deposit functions
    build: install flow repo via package.json
    test: add tests for FlowStreamCreator
    test: add tests for FlowBatchable

--- flow/FlowBatchable.sol
+++ flow/FlowBatchable.sol
@@ -5,16 +5,16 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { ud21x18, UD21x18 } from "@prb/math/src/UD21x18.sol";
 import { ud60x18, UD60x18 } from "@prb/math/src/UD60x18.sol";
 
-import { Broker, SablierFlow } from "../repos/flow/src/SablierFlow.sol";
+import { Broker, SablierFlow } from "@sablier/flow/src/SablierFlow.sol";
 
 /// @dev The `Batch` contract, inherited in SablierFlow, allows multiple function calls to be batched together.
 /// This enables any possible combination of functions to be executed within a single transaction.
 contract FlowBatchable {
-    IERC20 public constant USDC = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
-    SablierFlow public immutable sablierFlow;
+    IERC20 public constant USDC = IERC20(0xf08A50178dfcDe18524640EA6618a1f965821715);
+    SablierFlow public immutable SABLIER_FLOW;
 
     constructor(SablierFlow sablierFlow_) {
-        sablierFlow = sablierFlow_;
+        SABLIER_FLOW = sablierFlow_;
     }
 
     /// @dev A function to adjust the rate per second and deposits into a stream.
@@ -22,12 +22,18 @@ contract FlowBatchable {
         UD21x18 newRatePerSecond = ud21x18(0.0001e18);
         uint128 depositAmount = 1000e6;
 
+        // Transfer to this contract the amount to deposit in both streams.
+        USDC.transferFrom(msg.sender, address(this), depositAmount);
+
+        // Approve the Sablier contract to spend USDC
+        USDC.approve(address(SABLIER_FLOW), depositAmount);
+
         // The call data declared as bytes
         bytes[] memory calls = new bytes[](2);
-        calls[0] = abi.encodeCall(sablierFlow.adjustRatePerSecond, (streamId, newRatePerSecond));
-        calls[1] = abi.encodeCall(sablierFlow.deposit, (streamId, depositAmount));
+        calls[0] = abi.encodeCall(SABLIER_FLOW.adjustRatePerSecond, (streamId, newRatePerSecond));
+        calls[1] = abi.encodeCall(SABLIER_FLOW.deposit, (streamId, depositAmount, msg.sender, address(0xCAFE)));
 
-        sablierFlow.batch(calls);
+        SABLIER_FLOW.batch(calls);
     }
 
     /// @dev A function to create multiple streams in a single transaction.
@@ -40,17 +46,17 @@ contract FlowBatchable {
 
         // The call data declared as bytes
         bytes[] memory calls = new bytes[](2);
-        calls[0] = abi.encodeCall(sablierFlow.create, (sender, firstRecipient, firstRatePerSecond, USDC, true));
-        calls[1] = abi.encodeCall(sablierFlow.create, (sender, secondRecipient, secondRatePerSecond, USDC, true));
+        calls[0] = abi.encodeCall(SABLIER_FLOW.create, (sender, firstRecipient, firstRatePerSecond, USDC, true));
+        calls[1] = abi.encodeCall(SABLIER_FLOW.create, (sender, secondRecipient, secondRatePerSecond, USDC, true));
 
         // Prepare the `streamIds` array to return them
-        uint256 nextStreamId = sablierFlow.nextStreamId();
+        uint256 nextStreamId = SABLIER_FLOW.nextStreamId();
         streamIds = new uint256[](2);
         streamIds[0] = nextStreamId;
         streamIds[1] = nextStreamId + 1;
 
         // Execute multiple calls in a single transaction using the prepared call data.
-        sablierFlow.batch(calls);
+        SABLIER_FLOW.batch(calls);
     }
 
     /// @dev A function to create a stream and deposit via a broker in a single transaction.
@@ -66,15 +72,21 @@ contract FlowBatchable {
             fee: ud60x18(0.0001e18) // the fee percentage
          });
 
-        streamId = sablierFlow.nextStreamId();
+        // Transfer to this contract the amount to deposit in both streams.
+        USDC.transferFrom(msg.sender, address(this), depositAmount);
+
+        // Approve the Sablier contract to spend USDC
+        USDC.approve(address(SABLIER_FLOW), depositAmount);
+
+        streamId = SABLIER_FLOW.nextStreamId();
 
         // The call data declared as bytes
         bytes[] memory calls = new bytes[](2);
-        calls[0] = abi.encodeCall(sablierFlow.create, (sender, recipient, ratePerSecond, USDC, true));
-        calls[1] = abi.encodeCall(sablierFlow.depositViaBroker, (streamId, depositAmount, broker));
+        calls[0] = abi.encodeCall(SABLIER_FLOW.create, (sender, recipient, ratePerSecond, USDC, true));
+        calls[1] = abi.encodeCall(SABLIER_FLOW.depositViaBroker, (streamId, depositAmount, sender, recipient, broker));
 
         // Execute multiple calls in a single transaction using the prepared call data.
-        sablierFlow.batch(calls);
+        SABLIER_FLOW.batch(calls);
     }
 
     /// @dev A function to create multiple streams and deposit via a broker in a single transaction.
@@ -85,48 +97,57 @@ contract FlowBatchable {
         UD21x18 ratePerSecond = ud21x18(0.0001e18);
         uint128 depositAmount = 1000e6;
 
+        // Transfer the deposit amount of USDC tokens to this contract for both streams
+        USDC.transferFrom(msg.sender, address(this), 2 * depositAmount);
+
+        // Approve the Sablier contract to spend USDC
+        USDC.approve(address(SABLIER_FLOW), 2 * depositAmount);
+
         // The broker struct
         Broker memory broker = Broker({
             account: address(0xDEAD),
             fee: ud60x18(0.0001e18) // the fee percentage
          });
 
-        uint256 nextStreamId = sablierFlow.nextStreamId();
+        uint256 nextStreamId = SABLIER_FLOW.nextStreamId();
         streamIds = new uint256[](2);
         streamIds[0] = nextStreamId;
         streamIds[1] = nextStreamId + 1;
 
         // We need to have 4 different function calls, 2 for creating streams and 2 for depositing via broker
         bytes[] memory calls = new bytes[](4);
-        calls[0] = abi.encodeCall(sablierFlow.create, (sender, firstRecipient, ratePerSecond, USDC, true));
-        calls[1] = abi.encodeCall(sablierFlow.create, (sender, secondRecipient, ratePerSecond, USDC, true));
-        calls[2] = abi.encodeCall(sablierFlow.depositViaBroker, (sablierFlow.nextStreamId(), depositAmount, broker));
-        calls[3] = abi.encodeCall(sablierFlow.depositViaBroker, (sablierFlow.nextStreamId(), depositAmount, broker));
+        calls[0] = abi.encodeCall(SABLIER_FLOW.create, (sender, firstRecipient, ratePerSecond, USDC, true));
+        calls[1] = abi.encodeCall(SABLIER_FLOW.create, (sender, secondRecipient, ratePerSecond, USDC, true));
+        calls[2] =
+            abi.encodeCall(SABLIER_FLOW.depositViaBroker, (streamIds[0], depositAmount, sender, firstRecipient, broker));
+        calls[3] = abi.encodeCall(
+            SABLIER_FLOW.depositViaBroker, (streamIds[1], depositAmount, sender, secondRecipient, broker)
+        );
 
         // Execute multiple calls in a single transaction using the prepared call data.
-        sablierFlow.batch(calls);
+        SABLIER_FLOW.batch(calls);
     }
 
     /// @dev A function to pause a stream and withdraw the maximum available funds.
     function pauseAndWithdrawMax(uint256 streamId) external {
         // The call data declared as bytes
         bytes[] memory calls = new bytes[](2);
-        calls[0] = abi.encodeCall(sablierFlow.pause, (streamId));
-        calls[1] = abi.encodeCall(sablierFlow.withdrawMax, (streamId, address(0xCAFE)));
+        calls[0] = abi.encodeCall(SABLIER_FLOW.pause, (streamId));
+        calls[1] = abi.encodeCall(SABLIER_FLOW.withdrawMax, (streamId, address(0xCAFE)));
 
         // Execute multiple calls in a single transaction using the prepared call data.
-        sablierFlow.batch(calls);
+        SABLIER_FLOW.batch(calls);
     }
 
     /// @dev A function to void a stream and withdraw what is left.
     function voidAndWithdrawMax(uint256 streamId) external {
         // The call data declared as bytes
         bytes[] memory calls = new bytes[](2);
-        calls[0] = abi.encodeCall(sablierFlow.void, (streamId));
-        calls[1] = abi.encodeCall(sablierFlow.withdrawMax, (streamId, address(0xCAFE)));
+        calls[0] = abi.encodeCall(SABLIER_FLOW.void, (streamId));
+        calls[1] = abi.encodeCall(SABLIER_FLOW.withdrawMax, (streamId, address(0xCAFE)));
 
         // Execute multiple calls in a single transaction using the prepared call data.
-        sablierFlow.batch(calls);
+        SABLIER_FLOW.batch(calls);
     }
 
     /// @dev A function to withdraw maximum available funds from multiple streams in a single transaction.
@@ -136,11 +157,11 @@ contract FlowBatchable {
         // Iterate over the streamIds and prepare the call data for each stream
         bytes[] memory calls = new bytes[](count);
         for (uint256 i = 0; i < count; ++i) {
-            address recipient = sablierFlow.getRecipient(streamIds[i]);
-            calls[i] = abi.encodeCall(sablierFlow.withdrawMax, (streamIds[i], recipient));
+            address recipient = SABLIER_FLOW.getRecipient(streamIds[i]);
+            calls[i] = abi.encodeCall(SABLIER_FLOW.withdrawMax, (streamIds[i], recipient));
         }
 
         // Execute multiple calls in a single transaction using the prepared call data.
-        sablierFlow.batch(calls);
+        SABLIER_FLOW.batch(calls);
     }
 }
