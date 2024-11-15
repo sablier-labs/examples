2bfb5945af7a62d346fba8ff86a4004d5e648361
Andrei Vlad Birgaoanu <99738872+andreivladbrg@users.noreply.github.com>
2024-11-15 11:22:30 +0200

    feat: add staking contract example (#40)
    Co-authored-by: smol-ninja <shubhamy2015@gmail.com>

---
+++ v2/core/stake-sablier-nft-test/stake/stake.t.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.19;
+
+import { StakeSablierNFT_Fork_Test } from "../StakeSablierNFT.t.sol";
+
+contract Stake_Test is StakeSablierNFT_Fork_Test {
+    function test_RevertWhen_StreamingAssetIsNotRewardAsset() external {
+        resetPrank({ msgSender: users.bob.addr });
+
+        vm.expectRevert(abi.encodeWithSelector(DifferentStreamingAsset.selector, users.bob.streamId, DAI));
+        stakingContract.stake(users.bob.streamId);
+    }
+
+    modifier whenStreamingAssetIsRewardAsset() {
+        _;
+    }
+
+    function test_RevertWhen_AlreadyStaking() external whenStreamingAssetIsRewardAsset {
+        resetPrank({ msgSender: users.alice.addr });
+
+        vm.expectRevert(abi.encodeWithSelector(AlreadyStaking.selector, users.alice.addr, users.alice.streamId));
+        stakingContract.stake(users.alice.streamId);
+    }
+
+    modifier notAlreadyStaking() {
+        resetPrank({ msgSender: users.joe.addr });
+        _;
+    }
+
+    function test_Stake() external whenStreamingAssetIsRewardAsset notAlreadyStaking {
+        // Expect {Staked} event to be emitted.
+        vm.expectEmit({ emitter: address(stakingContract) });
+        emit Staked(users.joe.addr, users.joe.streamId);
+
+        // Stake the NFT.
+        stakingContract.stake(users.joe.streamId);
+
+        // Assertions: NFT has been transferred to the staking contract.
+        assertEq(SABLIER.ownerOf(users.joe.streamId), address(stakingContract));
+
+        // Assertions: storage variables.
+        assertEq(stakingContract.stakedAssets(users.joe.streamId), users.joe.addr);
+        assertEq(stakingContract.stakedTokenId(users.joe.addr), users.joe.streamId);
+
+        assertEq(stakingContract.totalERC20StakedSupply(), AMOUNT_IN_STREAM * 2);
+
+        // Assert: `updateReward` has correctly updated the storage variables.
+        assertApproxEqAbs(stakingContract.rewards(users.joe.addr), 0, 0);
+        assertEq(stakingContract.lastUpdateTime(), block.timestamp);
+        assertEq(stakingContract.totalRewardPaidPerERC20Token(), 0);
+        assertEq(stakingContract.userRewardPerERC20Token(users.joe.addr), 0);
+    }
+}
---
+++ v2/core/stake-sablier-nft-test/unstake/unstake.t.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.19;
+
+import { StakeSablierNFT_Fork_Test } from "../StakeSablierNFT.t.sol";
+
+contract Unstake_Test is StakeSablierNFT_Fork_Test {
+    function test_RevertWhen_CallerNotAuthorized() external {
+        // Change the caller to a non staker.
+        resetPrank({ msgSender: users.bob.addr });
+
+        vm.expectRevert(abi.encodeWithSelector(UnauthorizedCaller.selector, users.bob.addr, users.bob.streamId));
+        stakingContract.unstake(users.bob.streamId);
+    }
+
+    modifier whenCallerIsAuthorized() {
+        _;
+    }
+
+    modifier givenStaked() {
+        // Change the caller to a non staker and stake a stream.
+        resetPrank({ msgSender: users.joe.addr });
+        stakingContract.stake(users.joe.streamId);
+
+        vm.warp(block.timestamp + 1 days);
+        _;
+    }
+
+    function test_Unstake() external whenCallerIsAuthorized givenStaked {
+        // Expect {Unstaked} event to be emitted.
+        vm.expectEmit({ emitter: address(stakingContract) });
+        emit Unstaked(users.joe.addr, users.joe.streamId);
+
+        // Unstake the NFT.
+        stakingContract.unstake(users.joe.streamId);
+
+        // Assert: NFT has been transferred.
+        assertEq(SABLIER.ownerOf(users.joe.streamId), users.joe.addr);
+
+        // Assert: `stakedAssets` and `stakedTokenId` have been deleted from storage.
+        assertEq(stakingContract.stakedAssets(users.joe.streamId), address(0));
+        assertEq(stakingContract.stakedTokenId(users.joe.addr), 0);
+
+        // Assert: `totalERC20StakedSupply` has been updated.
+        assertEq(stakingContract.totalERC20StakedSupply(), AMOUNT_IN_STREAM);
+
+        // Assert: `updateReward` has correctly updated the storage variables.
+        uint256 expectedReward = 1 days * rewardRate / 2;
+        assertApproxEqAbs(stakingContract.rewards(users.joe.addr), expectedReward, 0.0001e18);
+        assertEq(stakingContract.lastUpdateTime(), block.timestamp);
+        assertEq(stakingContract.totalRewardPaidPerERC20Token(), (expectedReward * 1e18) / AMOUNT_IN_STREAM);
+        assertEq(stakingContract.userRewardPerERC20Token(users.joe.addr), (expectedReward * 1e18) / AMOUNT_IN_STREAM);
+    }
+}
